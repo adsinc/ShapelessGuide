b1f6753ec3a456eedfc118a7cdeb72d2570a683a
a.dolgiy <a.dolgiy@corp.mail.ru>
2018-11-27 09:35:54 +0300

    migration

--- src/main/scala/ch6/Ch6.scala
+++ src/main/scala/ch6/Ch6.scala
@@ -3,6 +3,9 @@ package ch6
 import ch6.Ch6.{IceCreamV1, IceCreamV2a, IceCreamV2b, IceCreamV2c}
 import shapeless._
 import shapeless.ops.hlist
+import cats._
+import cats.implicits._
+import shapeless.labelled.{FieldType, field}
 
 object Ch6 {
 
@@ -22,17 +25,38 @@ object Ch6 {
   }
 
   implicit def genericMigration[
-  A, B,
-  ARepr <: HList, BRepr <: HList,
-  Unaligned <: HList
+  A, B, ARepr <: HList, BRepr <: HList,
+  Common <: HList, Added <: HList, Unaligned <: HList
   ](
     implicit
     aGen: LabelledGeneric.Aux[A, ARepr],
     bGen: LabelledGeneric.Aux[B, BRepr],
-    inter: hlist.Intersection.Aux[ARepr, BRepr, Unaligned],
+    inter: hlist.Intersection.Aux[ARepr, BRepr, Common],
+    diff: hlist.Diff.Aux[BRepr, Common, Added],
+    monoid: Monoid[Added],
+    prepend: hlist.Prepend.Aux[Added, Common, Unaligned],
     align: hlist.Align[Unaligned, BRepr]
   ): Migration[A, B] =
-    a => bGen.from(align.apply(inter.apply(aGen.to(a))))
+    a => bGen.from(align(prepend(monoid.empty, inter(aGen.to(a)))))
+
+  def createMonoid[A](zero: A)(add: (A, A) => A): Monoid[A] =
+    new Monoid[A] {
+      override def empty: A = zero
+      override def combine(x: A, y: A): A = add(x, y)
+    }
+
+  implicit val hnilMonoid: Monoid[HNil] =
+    createMonoid[HNil](HNil)((_, _) => HNil)
+
+  implicit def emptyHlist[K <: Symbol, H, T <: HList] (
+    implicit
+    hMonoid: Lazy[Monoid[H]],
+    tMonoid: Monoid[T]
+  ): Monoid[FieldType[K, H] :: T] =
+    createMonoid(field[K](hMonoid.value.empty) :: tMonoid.empty) {
+      (x, y) =>
+        field[K](hMonoid.value.combine(x.head, y.head)) :: tMonoid.combine(x.tail, y.tail)
+    }
 }
 
 object Main extends App {
