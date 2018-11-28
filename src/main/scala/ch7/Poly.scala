dfa25c14bedf6a4bc58211149eeb98887fd72f57
a.dolgiy <a.dolgiy@corp.mail.ru>
2018-11-28 18:06:30 +0300

    poly

--- src/main/scala/ch7/Poly.scala
+++ src/main/scala/ch7/Poly.scala
@@ -3,6 +3,9 @@ package ch7
 import shapeless._
 import Poly._
 
+import scala.math.Numeric
+import scala.math.Numeric.Implicits._
+
 object Poly {
   object myPoly extends Poly1 {
     implicit val intCase: Case.Aux[Int, Double] = at(_ / 2.0)
@@ -18,6 +21,17 @@ object Poly {
       at((a, b) => b * a)
 
   }
+
+  object total extends Poly1 {
+    implicit def base[A](implicit num: Numeric[A]): Case.Aux[A, Double] =
+      at(_.toDouble())
+
+    implicit def option[A](implicit num: Numeric[A]): Case.Aux[Option[A], Double] =
+      at(opt => opt.map(num.toDouble).getOrElse(0))
+
+    implicit def list[A](implicit num: Numeric[A]): Case.Aux[List[A], Double] =
+      at(list => num.toDouble(list.sum))
+  }
 }
 
 object PolyMain extends App {
@@ -26,4 +40,9 @@ object PolyMain extends App {
 
   println(multiply(3, 4))
   println(multiply(3, "4"))
+
+  println(total(10))
+  println(total(Option(20.4)))
+
+  println(total(List(1L, 2L, 3L)))
 }
