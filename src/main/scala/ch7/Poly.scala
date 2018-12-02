28388cafce7dfa1c1d82c0432ccfaf4460d551ea
adsinc <sinc86@gmail.com>
2018-12-02 20:26:48 +0300

    poly

--- src/main/scala/ch7/Poly.scala
+++ src/main/scala/ch7/Poly.scala
@@ -32,6 +32,29 @@ object Poly {
     implicit def list[A](implicit num: Numeric[A]): Case.Aux[List[A], Double] =
       at(list => num.toDouble(list.sum))
   }
+
+  object sizeOf extends Poly1 {
+    implicit val intCase: Case.Aux[Int, Int] =
+      at(identity)
+
+    implicit val stringCase: Case.Aux[String, Int] =
+      at(_.length)
+
+    implicit val booleanCase: Case.Aux[Boolean, Int] =
+      at(b => if(b) 1 else 0)
+  }
+
+  object valueAndSizeOf extends Poly1 {
+    implicit val intCase: Case.Aux[Int, Int :: Int :: HNil] =
+      at(num => num :: num :: HNil)
+
+    implicit val stringCase: Case.Aux[String, String :: Int :: HNil] =
+      at(str => str :: str.length :: HNil)
+
+    implicit val booleanCase: Case.Aux[Boolean, Boolean :: Int :: HNil] =
+      at(b => b :: (if(b) 1 else 0) :: HNil)
+  }
+
 }
 
 object PolyMain extends App {
@@ -45,4 +68,7 @@ object PolyMain extends App {
   println(total(Option(20.4)))
 
   println(total(List(1L, 2L, 3L)))
+
+  println((10 :: "Hello" :: true :: HNil).map(sizeOf))
+  println((10 :: "Hello" :: true :: HNil).flatMap(valueAndSizeOf))
 }
