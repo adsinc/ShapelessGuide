312d802620eea19ee72e21ae961d904fd5598511
a.dolgiy <a.dolgiy@corp.mail.ru>
2018-12-03 17:52:37 +0300

    nat

--- src/main/scala/ch8/Ch8.scala
+++ src/main/scala/ch8/Ch8.scala
@@ -1,15 +1,35 @@
 package ch8
 
-import ch8.Ch8.Two
+import ch2.Ch2.IceCream
+import ch8.Ch8._
 import shapeless._
-import shapeless.ops.nat.ToInt
+import shapeless.ops.{coproduct, hlist, nat}
 
 object Ch8 {
-  type Zero = Nat._0
-  type One = Succ[Zero]
-  type Two = Succ[One]
+  val hlistLenght = hlist.Length[String :: Int :: Boolean :: HNil]
+  val coproductLenght = coproduct.Length[Double :+: Char :+: CNil]
+
+  trait SizeOf[A] {
+    def value: Int
+  }
+
+  def sizeOf[A](implicit size: SizeOf[A]): Int =
+    size.value
+
+  implicit def genericSizeOf[A, L <: HList, N <: Nat](
+    implicit
+    generic: Generic.Aux[A, L],
+    size: hlist.Length.Aux[L, N],
+    sizeToInt: nat.ToInt[N]
+  ): SizeOf[A] =
+    new SizeOf[A] {
+      val value: Int = sizeToInt.apply
+    }
 }
 
 object Main extends App {
-  println(ToInt[Two].apply())
+  println(Nat.toInt[hlistLenght.Out])
+  println(Nat.toInt[coproductLenght.Out])
+
+  println(sizeOf[IceCream])
 }
