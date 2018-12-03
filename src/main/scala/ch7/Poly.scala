f2e9952b0297c8bdda88d9b836e79356a272670c
adsinc <sinc86@gmail.com>
2018-12-03 08:27:32 +0300

    poly

--- src/main/scala/ch7/Poly.scala
+++ src/main/scala/ch7/Poly.scala
@@ -2,6 +2,7 @@ package ch7
 
 import shapeless._
 import Poly._
+import shapeless.ops.hlist
 
 import scala.math.Numeric
 import scala.math.Numeric.Implicits._
@@ -55,6 +56,48 @@ object Poly {
       at(b => b :: (if(b) 1 else 0) :: HNil)
   }
 
+  object sum extends Poly2 {
+    implicit val intIntCase: Case.Aux[Int, Int, Int] =
+      at((a, b) => a + b)
+
+    implicit val intStringCase: Case.Aux[Int, String, Int] =
+      at((a, b) => a + b.length)
+  }
+
+  trait ProductMapper[A, B, P] {
+    def apply(a: A): B
+  }
+
+  implicit def genericProductMapper[
+  A, B,
+  P <: Poly,
+  ARepr <: HList,
+  BRepr <: HList
+  ](
+    implicit
+    aGen: Generic.Aux[A, ARepr],
+    bGen: Generic.Aux[B, BRepr],
+    mapper: hlist.Mapper.Aux[P, ARepr, BRepr]
+  ): ProductMapper[A, B, P] =
+    a => bGen.from(mapper(aGen.to(a)))
+
+  implicit class ProductMapperOps[A](a: A) {
+    class Builder[B] {
+      def apply[P <: Poly](poly: P)(implicit pm: ProductMapper[A, B, P]): B =
+        pm(a)
+    }
+
+    def mapTo[B]: Builder[B] = new Builder[B]
+  }
+
+  object conversions extends Poly1 {
+    implicit val intCase: Case.Aux[Int, Boolean] = at(_ > 0)
+    implicit val boolCase: Case.Aux[Boolean, Int] = at(if(_) 1 else 0)
+    implicit val strCase: Case.Aux[String, String] = at(identity)
+  }
+
+  case class IceCream1(name: String, numCherries: Int, inCone: Boolean)
+  case class IceCream2(name: String, hasCherries: Boolean, numCones: Int)
 }
 
 object PolyMain extends App {
@@ -71,4 +114,8 @@ object PolyMain extends App {
 
   println((10 :: "Hello" :: true :: HNil).map(sizeOf))
   println((10 :: "Hello" :: true :: HNil).flatMap(valueAndSizeOf))
+
+  println((10 :: "hello" :: 100 :: HNil).foldLeft(0)(sum))
+
+  println(IceCream1("Sundae", 1, inCone = false).mapTo[IceCream2](conversions))
 }
